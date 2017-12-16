32b714b793d1ff8c60d184fa021f81ff384106ac
Luzi Leung <luzi.leung@gmail.com>
2017-12-16 18:12:00 +0800

    stat gacha result

--- python3/kirarafantasia_bot/state_list/gacha_result.py
+++ python3/kirarafantasia_bot/state_list/gacha_result.py
@@ -2,13 +2,20 @@ import os
 import sys
 import numpy as np
 import pygame
+import math
+import json
+import time
 
 from kirarafantasia_bot.image_recognition.gacha_result import classifier as gr_clr
 from clover.common import draw_util
+import clover.common
 from kirarafantasia_bot import bot
 import kirarafantasia_bot.state_list as state_common
 import clover.image_recognition
 from kirarafantasia_bot.image_recognition.gacha_result import setting as gr_setting
+
+NAME = 'gacha_result'
+HISTORY_FILE = 'gacha_result_history.txt'
 
 VIDEO_SIZE  = bot.VIDEO_SIZE
 TOUCH_SIZE  = bot.TOUCH_SIZE
@@ -22,17 +29,60 @@ def init(bot_logic):
 
     bot_logic.gr_clr = gr_clr.Classifier(gr_clr.MODEL_PATH)
     bot_logic.gr_clr.get(state_common.DUMMY_IMG)
+    
+    bot_logic.v[NAME] = {}
+    bot_logic.v[NAME]['last_img_box_list'] = cal_box_list(state_common.DUMMY_IMG)
+    bot_logic.v[NAME]['hi'] = -1.1244443326651827
+    bot_logic.v[NAME]['lo'] = -4.57894335851841
+
+    #gacha_result_stat = clover.common.read_json('gacha_result_stat.json')
+    gacha_result_stat = {
+        'single':[0]*3,
+        'bad_single':0,
+        'ten_s5':[0]*6,
+        'bad_ten':0,
+    }
+    if os.path.isfile(HISTORY_FILE):
+        for line in clover.common.readlines(HISTORY_FILE):
+            gacha_result = json.loads(line)
+            update_stat(gacha_result_stat, gacha_result)
+    bot_logic.v[NAME]['gacha_result_stat'] = gacha_result_stat
 
 def start(bot_logic, t):
-    bot_logic.gacha_result_cooldown_0 = t+3
-    bot_logic.gacha_result_cooldown_1 = t+6
-    bot_logic.gacha_result_cooldown_2 = t+9
+    bot_logic.gacha_result_cooldown_0 = t+2
+    bot_logic.gacha_result_cooldown_1 = t+99
+    bot_logic.gacha_result_cooldown_2 = t+999
 
 def tick(bot_logic, img, arm, t, ret):
     if t < bot_logic.gacha_result_cooldown_0:
         return False
     elif t < bot_logic.gacha_result_cooldown_1:
         label_list, score_list, perfect, ptp_list = bot_logic.gr_clr.get(img)
+        predict_good = [ (1 if (ptp_list[i]<=0)and(score_list[i]>=0.5) else 0) for i in range(len(label_list))]
+        
+        now_box_list = cal_box_list(img)
+        img_diff = np.average(np.absolute(now_box_list-bot_logic.v[NAME]['last_img_box_list']))
+        log_img_diff = math.log(img_diff)
+        if log_img_diff > -2.5:
+            bot_logic.v[NAME]['hi'] = min(bot_logic.v[NAME]['hi'],log_img_diff)
+        else:
+            bot_logic.v[NAME]['lo'] = max(bot_logic.v[NAME]['lo'],log_img_diff)
+        diff_mean = (bot_logic.v[NAME]['hi']+bot_logic.v[NAME]['lo'])/2
+
+        print('{}, {}, {}, {}, {}'.format(img_diff,log_img_diff,bot_logic.v[NAME]['hi'],bot_logic.v[NAME]['lo'],diff_mean))
+        bot_logic.v[NAME]['last_img_box_list'] = np.copy(now_box_list)
+
+        gacha_result_stat = bot_logic.v[NAME]['gacha_result_stat']
+        if log_img_diff > diff_mean:
+            gacha_result = {
+                'label_list': label_list,
+                'predict_good': predict_good,
+                'time':int(time.time()*1000),
+            }
+            clover.common.appendlines(HISTORY_FILE,[json.dumps(gacha_result)])
+            update_stat(gacha_result_stat, gacha_result)
+        
+        ret['gacha_result_stat'] = gacha_result_stat
 
         add_sample = False
         if not perfect:
@@ -45,7 +95,7 @@ def tick(bot_logic, img, arm, t, ret):
 
         ret['gacha_result_label_list'] = label_list
         ret['perfect'] = perfect
-        ret['ptp_list'] = ptp_list
+        ret['predict_good'] = predict_good
         
         s5count = sum([ 1 if i == 's5' else 0 for i in label_list ])
         bad_count = sum([ 1 if i > 0 else 0 for i in ptp_list ])
@@ -65,6 +115,9 @@ def tick(bot_logic, img, arm, t, ret):
             bot_logic.gacha_result_cooldown_0 = 0
             bot_logic.gacha_result_cooldown_1 = 0
             bot_logic.gacha_result_cooldown_2 = t+3
+        else:
+            bot_logic.gacha_result_cooldown_1 = t+99
+            bot_logic.gacha_result_cooldown_2 = t+999
         
 #        if (score >= 0) and (arm is not None):
 #            x = (btn_xywh[0]+btn_xywh[2]/2)*TOUCH_SIZE[0]/VIDEO_SIZE[0]
@@ -103,5 +156,74 @@ def draw(screen, tick_result):
             screen.blit(draw_util.text(gacha_result_label,(0,0,0)), (x-2,bot.VIDEO_SIZE[1]+y))
             screen.blit(draw_util.text(gacha_result_label,(0,0,0)), (x,bot.VIDEO_SIZE[1]+y+2))
             screen.blit(draw_util.text(gacha_result_label,(0,0,0)), (x,bot.VIDEO_SIZE[1]+y-2))
-            color = (0,255,0) if (tick_result['ptp_list'][i] <= 0) else (255,0,0)
+            color = (0,255,0) if (tick_result['predict_good'][i]>0) else (255,0,0)
             screen.blit(draw_util.text(gacha_result_label,color), (x,bot.VIDEO_SIZE[1]+y))
+
+    if 'gacha_result_stat' in tick_result:
+        gacha_result_stat = tick_result['gacha_result_stat']
+
+        y=60
+        single_sum = sum(gacha_result_stat['single'])
+        for i in range(len(gacha_result_stat['single'])):
+            star = 3+i
+            count = gacha_result_stat['single'][i]
+            ratio = count*100/single_sum
+            screen.blit(draw_util.text('s{0}: {1}, {2:.2f}%'.format(star,count,ratio),(0,0,0)), (bot.VIDEO_SIZE[0], y))
+            y+=20
+
+        screen.blit(draw_util.text('X: {0}'.format(gacha_result_stat['bad_single']),(0,0,0)), (bot.VIDEO_SIZE[0], y))
+        y+=20
+        y+=20
+        
+        ten_sum = sum(gacha_result_stat['ten_s5'])
+        for i in range(len(gacha_result_stat['ten_s5'])):
+            count = gacha_result_stat['ten_s5'][i]
+            if count > 0:
+                ratio = ten_sum/count
+                ratio = '{0:.02f}'.format(ratio)
+            else:
+                ratio = 'inf'
+            screen.blit(draw_util.text('s5 x{0}: {1}, 1/{2}'.format(i,count,ratio),(0,0,0)), (bot.VIDEO_SIZE[0], y))
+            y+=20
+        screen.blit(draw_util.text('X: {0}'.format(gacha_result_stat['bad_ten']),(0,0,0)), (bot.VIDEO_SIZE[0], y))
+        y+=20
+        y+=20
+
+# check for hardcode
+for _,_,w,h in BOUND_BOX_XYWH_LIST:
+    assert(w==55)
+    assert(h==55)
+
+def cal_box_list(img):
+    img_list = np.empty((len(BOUND_BOX_XYWH_LIST),55,55,3),np.float32)
+    for i in range(len(BOUND_BOX_XYWH_LIST)):
+        x,y,w,h = BOUND_BOX_XYWH_LIST[i]
+        xw, yh = x+w, y+h
+        img_list[i] = img[y:yh,x:xw,:]
+    return img_list
+
+def update_stat(gacha_result_stat, gacha_result):
+    label_list = gacha_result['label_list']
+    predict_good = gacha_result['predict_good']
+    perfect = True
+
+    ten_s5 = 0
+    for i in range(len(label_list)):
+        if predict_good[i]>0:
+            label_lv = 2 if label_list[i]=='s5' else \
+                       2 if label_list[i]=='d5' else \
+                       1 if label_list[i]=='s4' else \
+                       1 if label_list[i]=='d4' else \
+                       0
+            gacha_result_stat['single'][label_lv] += 1
+            if label_lv == 2:
+                ten_s5 += 1
+        else:
+            gacha_result_stat['bad_single'] += 1
+            perfect = False
+
+    ten_s5 = min(ten_s5,5)
+    if perfect:
+        gacha_result_stat['ten_s5'][ten_s5] += 1
+    else:
+        gacha_result_stat['bad_ten'] += 1

fa52d0e8b53de2a2a9db21e970bc2d88b03cbd4c
luzi82 <luzi82@gmail.com>
2017-12-19 13:00:48 +0800

    support state label change

--- python3/kirarafantasia_bot/image_recognition/state/add.py
+++ python3/kirarafantasia_bot/image_recognition/state/add.py
@@ -14,10 +14,34 @@ if __name__ == '__main__':
     parser.add_argument('--reset', action='store_true', help='clear and reset')
     args = parser.parse_args()
 
-    input_dir = 'input'
-    for label_name in os.listdir(input_dir):
-        label_path = os.path.join(input_dir,label_name)
-        path_list = []
+    INPUT_DIR = 'input'
+
+    # get label list
+    label_state_path = os.path.join('image_recognition','label','state')
+    label_name_list = os.listdir(label_state_path)
+    label_name_list = filter(lambda v:os.path.isfile(os.path.join(label_state_path,v)),label_name_list)
+    label_name_list = filter(lambda v:v.endswith('.txt'),label_name_list)
+    label_name_list = [ i[:-4] for i in label_name_list]
+    label_name_list = set(label_name_list)
+    label_name_list = label_name_list | set([i for i in os.listdir(INPUT_DIR) if os.path.isdir(i)])
+    label_name_list = sorted(list(label_name_list))
+    
+    # read old label
+    fn_label_dict = {}
+    if not args.reset:
+        for label_name in label_name_list:
+            label_txt_path = os.path.join('image_recognition','label','state','{}.txt'.format(label_name))
+            if not os.path.isfile(label_txt_path):
+                continue
+            fn_list = clover.common.readlines(label_txt_path)
+            for fn in fn_list:
+                if not os.path.isfile(fn):
+                    continue
+                fn_label_dict[fn] = label_name
+
+    # read dir
+    for label_name in os.listdir(INPUT_DIR):
+        label_path = os.path.join(INPUT_DIR,label_name)
         if not os.path.isdir(label_path):
             continue
         for image_fn in os.listdir(label_path):
@@ -30,24 +54,17 @@ if __name__ == '__main__':
             if not os.path.isfile(image_ori_path):
                 print('{} not found'.format(image_ori_path),file=sys.stderr)
                 continue
-            path_list.append(image_ori_path)
-            #print('{} {} {}'.format(image_timestamp,image_idx,label_name))
+            fn_label_dict[image_ori_path] = label_name
 
-        label_path = os.path.join('image_recognition','label','state','{}.txt'.format(label_name))
-
-        path_list_ori = []
-        if not args.reset:
-            if os.path.isfile(label_path):
-                with open(label_path, mode='rt', encoding='utf-8') as fin:
-                    path_list_ori = fin.readlines()
-                path_list_ori = [ i.strip() for i in path_list_ori ]
-
-        path_list = path_list + path_list_ori
-        path_list = list(set(path_list))
+    # output txt file
+    for label_name in label_name_list:
+        path_list = []
+        for fn, label in fn_label_dict.items():
+            if label != label_name:
+                continue
+            path_list.append(fn)
         path_list = sorted(path_list)
 
+        label_txt_path = os.path.join('image_recognition','label','state','{}.txt'.format(label_name))
         clover.common.makedirs(os.path.join('image_recognition','label','state'))
-
-        with open(label_path, mode='wt', encoding='utf-8') as fout:
-            for path in path_list:
-                fout.write('{}\n'.format(path))
+        clover.common.writelines(label_txt_path,path_list)

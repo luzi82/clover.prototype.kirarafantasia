dc73dabeb0183acc65ed91ff076d0d6a419f26e4
Luzi82 <luzi82@gmail.com>
2018-01-07 15:52:39 +0800

    free mem

--- python3/kirarafantasia_bot/image_recognition/state/train.py
+++ python3/kirarafantasia_bot/image_recognition/state/train.py
@@ -75,93 +75,37 @@ if __name__ == '__main__':
 
     assert(((args.epochs!=None)and(args.mirror_count!=None))or(args.summaryonly))
 
-    # get label list
-    label_state_path = os.path.join('image_recognition','label','state')
-    label_name_list = os.listdir(label_state_path)
-    label_name_list = filter(lambda v:os.path.isfile(os.path.join(label_state_path,v)),label_name_list)
-    label_name_list = filter(lambda v:v.endswith('.txt'),label_name_list)
-    label_name_list = [ i[:-4] for i in label_name_list]
-    label_name_list = sorted(label_name_list)
-    
-    label_count = len(label_name_list)
+    now = int(time.time())
 
-    # fast quit if summaryonly
-    if args.summaryonly:
-        from . import model as model_setting
-        model = model_setting.create_model(label_count)
-        model.summary()
-        quit()
-
-    # clear session
-    SESSION_CACHE_DIR_PATH = os.path.join('cache','session','state.train')
-    clover.common.reset_dir(SESSION_CACHE_DIR_PATH)
-
-    # load sample list
-    sample_list = []
-    for label_idx in range(label_count):
-        label_name = label_name_list[label_idx]
-        img_fn_list_fn = os.path.join(label_state_path,'{}.txt'.format(label_name))
-        with open(img_fn_list_fn, mode='rt', encoding='utf-8') as fin:
-            img_fn_list = fin.readlines()
-        img_fn_list = [ img_fn.strip() for img_fn in img_fn_list ]
-        sample_list += [{'fn':img_fn, 'label_idx':label_idx, 'label_name': label_name} for img_fn in img_fn_list ]
-
-    # randomize sample order
-    random.shuffle(sample_list)
-    #sample_list = sample_list[:100]
-
-    # clean dir
-    clover.common.reset_dir(os.path.join('image_recognition','model','state'))
-
-    # write data
-    j = {
-        'label_name_list': label_name_list,
-        'mirror_count':    args.mirror_count
-    }
-    with open(os.path.join('image_recognition','model','state','data.json'),'w') as fout:
-        json.dump(j, fp=fout, indent=2, sort_keys=True)
-        fout.write('\n')
-
-    test_sample_count        = int(len(sample_list)/10)
-    test_sample_list         = sample_list[:test_sample_count]
-    train_valid_sample_list  = sample_list[test_sample_count:]
-    train_valid_sample_count = len(train_valid_sample_list)
-
-    # load data set, save to cache
-    img_list, label_onehot_list = sample_list_to_data_set(train_valid_sample_list,label_count)
-    train_valid_sample_data_dir_path = os.path.join(SESSION_CACHE_DIR_PATH,'train_valid_sample_data')
-    save_data_set(train_valid_sample_data_dir_path, img_list, label_onehot_list)
-    del img_list, label_onehot_list
-
-    train_unit_data = {
-        'label_count':                      label_count,
-        'sample_list':                      sample_list,
-        'train_valid_sample_data_dir_path': train_valid_sample_data_dir_path,
-        'test_sample_count':                test_sample_count,
-        'epochs':                           args.epochs,
-        'mirror_count':                     args.mirror_count,
-        'mirror_data_list':                 [],
-    }
-
-    for mirror_idx in range(args.mirror_count):
-
-        hdf5_fn = os.path.join('image_recognition','model','state','weight.{}.hdf5'.format(mirror_idx))
-
-        valid_start = int(train_valid_sample_count*(mirror_idx+0)/float(args.mirror_count))
-        valid_end   = int(train_valid_sample_count*(mirror_idx+1)/float(args.mirror_count))
-        
-        mirror_data = {
-            'hdf5_fn': hdf5_fn,
-            'valid_start': valid_start,
-            'valid_end': valid_end,
-        }
-        train_unit_data['mirror_data_list'].append(mirror_data)
-        
     TRAIN_UNIT_PATH_FORMAT = '/tmp/IWNFWFKRMF-{}.json'
-    train_unit_path = TRAIN_UNIT_PATH_FORMAT.format(int(time.time()))
-    clover.common.write_json(train_unit_path,train_unit_data)
+    train_unit_path = TRAIN_UNIT_PATH_FORMAT.format(now)
     
-    gc.collect()
+    TRAIN_PREPARE_PATH_FORMAT = '/tmp/MGOKDVXPAS-{}.json'
+    train_prepare_path = TRAIN_PREPARE_PATH_FORMAT.format(now)
+    
+    train_prepare_data = {
+        'train_unit_path':  train_unit_path,
+        'summaryonly':      args.summaryonly,
+        'mirror_count':     args.mirror_count,
+        'epochs':           args.epochs,
+    }
+    clover.common.write_json(train_prepare_path,train_prepare_data)
+    
+    proc = subprocess.Popen([
+            sys.executable,
+            '-m','kirarafantasia_bot.image_recognition.state._train_prepare',
+            train_prepare_path,
+        ],
+        stderr=None,
+        stdout=None
+    )
+    ret_code = proc.wait()
+    if ret_code != 0:
+        print('_train_prepare quit with err code: {}'.format(ret_code))
+        exit(ret_code)
+        
+    if args.summaryonly:
+        quit()
     
     for mirror_idx in range(args.mirror_count):
         proc = subprocess.Popen([

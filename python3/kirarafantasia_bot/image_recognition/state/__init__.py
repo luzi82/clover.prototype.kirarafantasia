ed4e11e2c8146be3d53ae90fc8fd4cb672784358
luzi82 <luzi82@gmail.com>
2018-01-23 17:40:37 +0800

    sorted seems non-reusable

--- python3/kirarafantasia_bot/image_recognition/state/__init__.py
+++ python3/kirarafantasia_bot/image_recognition/state/__init__.py
@@ -7,7 +7,7 @@ def get_label_list():
     label_name_list = filter(lambda v:os.path.isfile(os.path.join(label_state_path,v)),label_name_list)
     label_name_list = filter(lambda v:v.endswith('.txt'),label_name_list)
     label_name_list = [ i[:-4] for i in label_name_list]
-    label_name_list = sorted(label_name_list)
+    label_name_list = list(sorted(label_name_list))
     return label_name_list
 
 def get_known_state(fn):

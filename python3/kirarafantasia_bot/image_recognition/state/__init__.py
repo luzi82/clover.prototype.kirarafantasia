ca89f2410c564750738590386d83a17415defd70
luzi82 <luzi82@gmail.com>
2018-01-23 17:43:42 +0800

    better code reuse

--- python3/kirarafantasia_bot/image_recognition/state/__init__.py
+++ python3/kirarafantasia_bot/image_recognition/state/__init__.py
@@ -1,10 +1,11 @@
 import os
 import clover.common
 
+LABEL_STATE_PATH = os.path.join('image_recognition','label','state')
+
 def get_label_list():
-    label_state_path = os.path.join('image_recognition','label','state')
-    label_name_list = os.listdir(label_state_path)
-    label_name_list = filter(lambda v:os.path.isfile(os.path.join(label_state_path,v)),label_name_list)
+    label_name_list = os.listdir(LABEL_STATE_PATH)
+    label_name_list = filter(lambda v:os.path.isfile(os.path.join(LABEL_STATE_PATH,v)),label_name_list)
     label_name_list = filter(lambda v:v.endswith('.txt'),label_name_list)
     label_name_list = [ i[:-4] for i in label_name_list]
     label_name_list = list(sorted(label_name_list))

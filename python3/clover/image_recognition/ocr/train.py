34c320715e0ff123103d66279236b9b8f1b0e477
luzi82 <luzi82@gmail.com>
2018-02-06 19:47:25 +0800

    lots of code...

--- python3/clover/image_recognition/ocr/train.py
+++ python3/clover/image_recognition/ocr/train.py
@@ -1,6 +1,17 @@
 import captcha.image as captcha_image
 import os
 from . import draw_text
+import time
+from . import model
+from keras.layers import Input, Lambda
+from keras import backend as K
+from keras.optimizers import SGD
+from keras.callbacks import ModelCheckpoint, CSVLogger
+from keras.models import Model
+import clover.common
+import numpy as np
+import random
+import cv2
 
 class TextImageGenerator():
 
@@ -21,6 +32,7 @@ class TextImageGenerator():
         self.img_h1_max = img_h1_max
         self.img_w2 = img_w2
         self.img_h2 = img_h2
+        self.img_w2b = img_w2b
         self.char_set = char_set
         self.blank_label = len(char_set)
         self.max_word_len = max_word_len
@@ -39,8 +51,8 @@ class TextImageGenerator():
             word = ''.join(random.choice(self.char_set) for _ in range(word_len))
 
             h0 = self.img_h0
-            w0 = self.char_w * word_len
-            w0 = min(width, self.img_w0)
+            w0 = self.char_w0 * word_len
+            w0 = min(w0, self.img_w0)
             w0 = random.randint(w0, self.img_w0)
             img = self.draw_text.create_image(word, w0, h0)
             img = np.asarray(img,dtype=np.float32)
@@ -51,13 +63,13 @@ class TextImageGenerator():
             
             h1 = random.randint(self.img_h1_min, self.img_h1_max)
             w1 = round(w0*h1/h0)
-            img = cv2.resize(img,dsize(w1,h1))
+            img = cv2.resize(img,dsize=(w1,h1))
             assert(img.shape==(h1,w1,3))
             
             h2 = self.img_h2
             w2 = round(w0*h2/h0)
-            w2 = min(w2,self.img_w)
-            img = cv2.resize(img,dsize(w2,h2))
+            w2 = min(w2,self.img_w2)
+            img = cv2.resize(img,dsize=(w2,h2))
             assert(img.shape==(h2,w2,3))
 
             img = np.transpose(img,(1,0,2))
@@ -65,10 +77,10 @@ class TextImageGenerator():
 
             X_data[i, self.img_w2b:self.img_w2b+w2, :, 0:3] = img
             X_data[i, self.img_w2b:self.img_w2b+w2, :, 3] = 1
-            labels[i, 0:word_len] = text_to_labels(word)
+            labels[i, 0:word_len] = text_to_labels(word, self.char_set)
             input_length[i] = w2
             label_length[i] = word_len
-            source_str.append(word)
+            #source_str.append(word)
         inputs = {
             'the_input': X_data,
             'the_labels': labels,
@@ -78,12 +90,7 @@ class TextImageGenerator():
         outputs = {'ctc': np.zeros([size])}  # dummy data for dummy loss function
         return (inputs, outputs)
 
-    def next_train(self):
-        while 1:
-            ret = self.get_batch(self.minibatch_size)
-            yield ret
-
-    def next_val(self):
+    def next_batch(self):
         while 1:
             ret = self.get_batch(self.minibatch_size)
             yield ret
@@ -94,6 +101,19 @@ class TextImageGenerator():
     def get_model_input_shape(self):
         return (self.img_w2+self.img_w2b*2, self.img_h2, 4)
 
+def ctc_lambda_func_factory(img_w2b):
+    def ctc_lambda_func(args):
+        y_pred, labels, input_length, label_length = args
+        y_pred = y_pred[:, img_w2b:-img_w2b, :]
+        return K.ctc_batch_cost(labels, y_pred, input_length, label_length)
+    return ctc_lambda_func
+
+def text_to_labels(text, chars):
+    ret = []
+    for char in text:
+        ret.append(chars.find(char))
+    return ret
+
 OUTPUT_DIR = os.path.join('image_recognition','model','ocr')
 
 if __name__ == '__main__':
@@ -102,13 +122,16 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='ocr trainer')
     parser.add_argument('epochs', type=int, help="epochs")
     args = parser.parse_args()
-    
+
+    output_dir = OUTPUT_DIR
+    clover.common.reset_dir(output_dir)
+
     timestamp = str(int(time.time()))
     minibatch_size = 32
     train_count = 3200
     val_count = 320
-    output_dir = os.path.join(OUTPUT_DIR, timestamp)
     char_set='0123456789'
+    max_word_len = 10
     
     img_gen = TextImageGenerator(
         minibatch_size=minibatch_size,
@@ -116,20 +139,20 @@ if __name__ == '__main__':
         img_h1_min=6, img_h1_max=40,
         img_w2=100, img_h2=16, img_w2b=8,
         char_set=char_set,
-        max_word_len=10
+        max_word_len=max_word_len
     )
     
     tensor_in, tensor_out = model.tensor_in_out(img_gen.get_label_count(),img_gen.get_model_input_shape())
 
-    labels = Input(name='the_labels', shape=[img_gen.absolute_max_string_len], dtype='float32')
+    labels = Input(name='the_labels', shape=[max_word_len], dtype='float32')
     input_length = Input(name='input_length', shape=[1], dtype='int64')
     label_length = Input(name='label_length', shape=[1], dtype='int64')
 
-    loss_out = Lambda(ctc_lambda_func, output_shape=(1,), name='ctc')([y_pred, labels, input_length, label_length])
+    loss_out = Lambda(ctc_lambda_func_factory(8), output_shape=(1,), name='ctc')([tensor_out, labels, input_length, label_length])
 
     sgd = SGD(lr=0.02, decay=1e-6, momentum=0.9, nesterov=True, clipnorm=5)
 
-    model = Model(inputs=[input_data, labels, input_length, label_length], outputs=loss_out)
+    model = Model(inputs=[tensor_in, labels, input_length, label_length], outputs=loss_out)
 
     # the loss calc occurs elsewhere, so use a dummy lambda func for the loss
     model.compile(loss={'ctc': lambda y_true, y_pred: y_pred}, optimizer=sgd)
@@ -144,5 +167,5 @@ if __name__ == '__main__':
                         validation_data=img_gen.next_batch(),
                         validation_steps=val_count // minibatch_size,
                         callbacks=[model_checkpoint, model_best, csv_logger],
-                        verbose=verbose
+                        verbose=2
                         )
